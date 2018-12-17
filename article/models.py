bc22c74aa607622d309db10e05ed17650a9f30b3
tai45208 <tai45208@outlook.com>
2018-12-17 15:22:56 +0800

    c.7

---
+++ article/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.article.title + '-' + str(self.id)
